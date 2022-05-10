0c138df93922b692f9418687b096c14353774494
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-05-10 03:12:00 -0500

    complete src-sw file

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,6 +5,7 @@ const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 const { StaleWhileRevalidate } = require("workbox-strategies");
+const { request } = require("express");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -37,6 +38,18 @@ registerRoute(
     cacheName: "asset-cache",
     plugins: [
       // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new StaleWhileRevalidate({
+    cacheName: "image-cache",
+    plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
